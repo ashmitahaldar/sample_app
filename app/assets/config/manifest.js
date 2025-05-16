3109787db7e07a97e4023a1f064e6be64559fc9f
ashmitahaldar <ashmita.haldar@yahoo.com>
2025-05-16 13:05:00 +0700

    Implement basic login

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= require bootstrap-sprockets
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
