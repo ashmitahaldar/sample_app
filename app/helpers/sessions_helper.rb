2ecef26cc56b610d324466ceb86aa87824214683
ashmitahaldar <ashmita.haldar@yahoo.com>
2025-05-19 15:42:33 +0700

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,14 +16,11 @@ module SessionsHelper
   # returns the current logged-in user (if any)
   def current_user
     if (user_id = session[:user_id])
-      # @current_user ||= User.find_by(id: user_id)
       user = User.find_by(id: user_id)
-      if user && session[:session_token] == user.session_token
-        @current_user = user
-      end
+      @current_user ||= user if session[:session_token] == user.session_token
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
