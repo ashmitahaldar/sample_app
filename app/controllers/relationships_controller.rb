fe45e7ac3e65fedbdb6cf3030bc18a3842b86a55
ashmitahaldar <ashmita.haldar@yahoo.com>
2025-05-21 08:47:50 +0700

    Disabled hotwire/turbo for follow/unfollow feature to remove bugs

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,18 +4,22 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.turbo_stream
-    end
+    redirect_to user
+    # Implementing Hotwire/Turbo causing bugs
+    # respond_to do |format|
+    #   format.html { redirect_to @user }
+    #   format.turbo_stream
+    # end
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
-    respond_to do |format|
-      format.html { redirect_to @user, status: :see_other }
-      format.turbo_stream
-    end
+    redirect_to user, status: :see_other
+    # Implementing Hotwire/Turbo causing bugs
+    # respond_to do |format|
+    #   format.html { redirect_to @user, status: :see_other }
+    #   format.turbo_stream
+    # end
   end
 end
